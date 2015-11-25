2d85c8be42aa6f07b3f512a1722c6be70807aa7e
Janson Kang <temasek71@gmail.com>
2015-11-25 17:43:41 +0800

    hlte-common: kang izat.conf from jf

--- hlte.mk
+++ hlte.mk
@@ -73,7 +73,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/gps.conf:/system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/flp.conf:/system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/sap.conf:/system/etc/sap.conf
+    $(LOCAL_PATH)/gps/etc/sap.conf:/system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:/system/etc/izat.conf
 
 # Input device
 PRODUCT_COPY_FILES += \
