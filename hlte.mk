e45766be45cf36132d5392d685ac2cdda79a2f47
Joshndroid <fmxjoshiewah@gmail.com>
2015-11-26 15:28:25 +1000

    Fix numerator error - full screen bootanimation

--- hlte.mk
+++ hlte.mk
@@ -31,7 +31,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_ASPECT_RATIO := 16-by-9
+TARGET_SCREEN_ASPECT_RATIO := 16by9
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
