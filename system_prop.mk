c1364c82b98aba313fbc2f3f0968d88957228d47
Janson Kang <temasek71@gmail.com>
2016-01-03 19:07:58 +0800

    hlte-common: Set dalvik filter to speed

--- system_prop.mk
+++ system_prop.mk
@@ -1,5 +1,7 @@
 # Art
-dalvik.vm.dex2oat-swap=false
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.dex2oat-swap=false
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
