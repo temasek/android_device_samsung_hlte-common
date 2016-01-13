60fa21e090d759ee7d35a87790876aa0d2dd16ad
Janson Kang <temasek71@gmail.com>
2016-01-13 15:58:59 +0800

    ril: Revamp ril for M
    * Removal of Operators requirement
    * Clean up Android.mk for shim
    * Overhaul ril (using klte as reference)

--- ril/Android.mk
+++ ril/Android.mk
@@ -11,15 +11,3 @@ LOCAL_MODULE := libril_shim
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
-
-LOCAL_SRC_FILES := \
-    rmt_storage_shim.c
-
-LOCAL_MODULE := librmt_shim
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
-include $(BUILD_SHARED_LIBRARY)
