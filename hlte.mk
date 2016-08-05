209c91262c323a422e6819a641b5727f01e4ee50
USBhost <josiahspore@gmail.com>
2016-08-05 00:03:44 -0700

    add com.google.android.gms.persistent killing script
    Signed-off-by: USBhost <josiahspore@gmail.com>

--- hlte.mk
+++ hlte.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    GSM.sh
 
 # Thermal
 PRODUCT_COPY_FILES += \
